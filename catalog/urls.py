6dffa9368c507ca0d44b6e9a46ddd986cb796223
lutovinova_mariya <bukovka09@gmail.com>
2026-08-28 13:17:29 +0300

    Fix: Correct admin urls syntax and verify catalog app connection

---
+++ catalog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+# Здесь будут храниться маршруты для каталога товаров
+urlpatterns = [
+    # Пример пустого маршрута (главная страница каталога)
+    # path('', views.home, name='home'),
+]
